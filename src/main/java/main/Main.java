eeb30d6bd9b19bf9c7754991872ab9114ae91cd3
Sergio Martí <sergio.martihee1997@gmail.com>
2023-01-25 14:26:37 +0100

    [START] start project

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/mainView.fxml")));
+        primaryStage.setTitle("Car Crime Game");
+        primaryStage.setScene(new Scene(root, 1280, 725));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
